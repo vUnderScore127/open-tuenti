0486a300b5a45839ced3bf3f932f4e7745d42f50
Chopo <chopo@example.com>
2025-10-29 23:34:44 +0100

    Corregir routing para GitHub Pages - configurar basename en BrowserRouter

---
+++ dist/assets/status-tap-BWeJR3-V.js
@@ -0,0 +1,3 @@
+import{r as i,f as a,b as c,w as d,s as l}from"./index-BWKX_4_d.js";import"./vendor-BFiIXK5m.js";import"./supabase-Bh3ylEV3.js";/*!
+ * (C) Ionic http://ionicframework.com - MIT License
+ */const f=()=>{const e=window;e.addEventListener("statusTap",()=>{i(()=>{const n=e.innerWidth,s=e.innerHeight,o=document.elementFromPoint(n/2,s/2);if(!o)return;const t=a(o);t&&new Promise(r=>c(t,r)).then(()=>{d(async()=>{t.style.setProperty("--overflow","hidden"),await l(t,300),t.style.removeProperty("--overflow")})})})})};export{f as startStatusTap};
